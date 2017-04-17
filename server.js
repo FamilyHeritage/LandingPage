8cc67d7db59e7ae577679ed98a7bec685a27ee9d
DESKTOP-UMT0L75\kishan yadav <connect.familyheritage@outlook.com>
2017-04-17 21:57:26 +0200

    signup working fine with index.html rendered

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ var port =3000;
 var userRouter = express.Router();
 var mysql   = require("mysql");
 var md5 = require('MD5');
+var ejs = require("ejs");
+var path = require("path")
 var bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
-
-
-  userRouter.route('/users')
-      .get(function(req,res){
-          var responsejson={hello:"this is my user"};
-          res.json(responsejson);
-      }); 
-
 
    var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -30,14 +24,20 @@ connection.connect();
 
 app.set('view engine', 'views');
 app.use('/api',userRouter); 
-app.use(express.static("css"))
-app.use(express.static("images"))
+app.use(express.static("../landingpage/css"))
+app.use(express.static("../landingpage/images"))
+app.use(express.static("../landingpage/views/js"))
+app.use(bodyparser.urlencoded({
+    extended : true
+}))
+app.use(bodyparser.json())
 
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) { 
-    res.render('pages/index.ejs');
+    res.sendFile(path.join (__dirname + "/views/pages/index.html"));
 });
-
+console.log("dirname", __dirname)
 app.get('/login', function(req, res) {  
     var post = { 
   first_name: 'satyam',
@@ -51,12 +51,32 @@ app.get('/login', function(req, res) {
  // if (error) throw error; 
 
  // console.log('The solution is: ', results[0]);
-//}); 
+//});  
+
  connection.query('INSERT INTO user_signup SET ?', post, function (err, result)  { 
       console.log('connection done', result)
 });
     //console.log('result is ', res)
 });
+
+app.post("/signup", function(req, res) {
+    console.log(req.body) 
+    var user = {
+     first_name: req.body.first_name,
+     last_name: req.body.last_name,
+     email: req.body.email,
+     password: req.body.password,
+     active: 'y' 
+    }
+    
+     connection.query('INSERT INTO user_signup SET ?', user, function (err, result)  { 
+      console.log('connection done', result) 
+      if(result) {
+          res.sendfile(path.join (__dirname + "/views/pages/welcome.html"))
+      }
+    });
+
+})
 
 app.listen(port,function(){
     console.log('running on port'+port);
